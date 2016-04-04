dd245b046652e38be074e5d63e2f8c0758fae857
Łukasz Witaszczyk <lukasz.witaszczyk@gmail.com>
2016-04-04 23:31:56 +0200

    qsf [WIP] as component - tests

--- src/Html/Label.php
+++ src/Html/Label.php
@@ -5,11 +5,6 @@ namespace Html;
 class Label extends Tag
 {
     /**
-     * @var Control
-     */
-    private $control;
-
-    /**
      * @param string $content
      */
     public function __construct($content = '')
